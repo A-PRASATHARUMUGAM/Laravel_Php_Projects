11dc466b261d2e504ce0d079252896569598b788
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-09-11 09:49:23 +0530

    fix: add required attributes to form inputs for validation

--- Form_Project/index.php
+++ Form_Project/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
-    <title>Validation Form Project </title>
+    <trequireditle>Validation Form Project </trequireditle>
 </head>
 <body>
     
@@ -19,25 +19,25 @@
           <!-- input1  -->
           <div>
               <label for="username" >Username</label> <br>
-              <input type="text" name="username" id="username" >
+              <input type="text" name="username" id="username" required>
               <div class="error"></div>
           </div>
           <!-- input2  -->
           <div>
               <label for="Email" >Email</label><br>
-              <input type="email" name="email" id="email" >
+              <input type="email" name="email" id="email"required >
               <div class="error"></div>
           </div>
           <!-- input3  -->
           <div>
               <label for="Password" >Password</label><br>
-              <input type="password" name="new-password" id="password" >
+              <input type="password" name="new-password" id="password" required>
               <div class="error"></div>
           </div>
           <!-- input4  -->
           <div>
               <label for="Conform Password" >Confirm Password</label><br>
-              <input type="password" name="cpassword" id="cpassword"  >
+              <input type="password" name="cpassword" id="cpassword" required >
               <div class="error"></div>
 
           </div>
